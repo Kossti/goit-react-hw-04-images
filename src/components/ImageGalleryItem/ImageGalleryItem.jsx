dd6c8800c1e8895aee04d6cd0e38bd5619bd6386
Kossti <timoshenko.konstantin@gmail.com>
2023-05-25 21:26:43 +0300

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,42 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'components/Modal/Modal';
 import css from './ImageGalleryItem.module.css';
 
-export default class ImageGalleryItem extends Component {
-  state = {
-    statusModal: false,
+function ImageGalleryItem({ item }) {
+  const [statusModal, setStatusModal] = useState(false);
+  const { id, webformatURL, largeImageURL, type } = item;
+
+  const toggleModal = () => {
+    setStatusModal(!statusModal);
   };
 
-  toggleModal = () => {
-    this.setState(({ statusModal }) => ({
-      statusModal: !statusModal,
-    }));
-  };
-
-  render() {
-    const { id, webformatURL, largeImageURL, type } = this.props.item;
-
-    return (
-      <>
-        <li key={id} className={css.ImageGalleryItem}>
-          <img
-            className={css.ImageGalleryItemImage}
-            id={id}
-            src={webformatURL}
-            alt={type}
-            onClick={this.toggleModal}
-          />
-        </li>
-        {this.state.statusModal && (
-          <Modal onClose={this.toggleModal} image={largeImageURL}>
-            <img src={largeImageURL} alt="largeImage" />
-          </Modal>
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      <li key={id} className={css.ImageGalleryItem}>
+        <img
+          className={css.ImageGalleryItemImage}
+          // id={id}
+          src={webformatURL}
+          alt={type}
+          onClick={toggleModal}
+        />
+      </li>
+      {statusModal && (
+        <Modal onClose={toggleModal} image={largeImageURL}>
+          <img src={largeImageURL} alt="largeImage" />
+        </Modal>
+      )}
+    </>
+  );
 }
+
+export default ImageGalleryItem;
 
 ImageGalleryItem.propTypes = {
   item: PropTypes.shape({
